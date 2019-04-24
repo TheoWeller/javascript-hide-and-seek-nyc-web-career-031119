aff9c90bc279ec37689fcb9c56fc5f7b0deac7cd
Theo Weller <weller.theodor@gmail.com>
2019-04-24 00:36:41 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+const app = document.getElementById("app");
+// const getFirstSelector = app.querySelector('ul.ranked-list li')
 
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+function deepestChild() {
+  return document.querySelector('#grand-node div div div div');
+}
+
+function increaseRankBy(n) {
+   let list = document.querySelector('.ranked-list');
+   list = list.children
+   for (const item of list) {item.innerText = item.innerText + n }
+}
