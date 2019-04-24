17159adeb567b73bd24e3478680cc5722ea6a9c7
Theo Weller <weller.theodor@gmail.com>
2019-04-24 00:05:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const app = document.getElementById("app");
 // const getFirstSelector = app.querySelector('ul.ranked-list li')
 
 function getFirstSelector(selector) {
-  document.querySelector(selector);
+  return document.querySelector(selector);
 
 }
